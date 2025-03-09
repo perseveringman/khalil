2d1a082e171b0f54dea77cff51924831c262ddbc
ryanbzhou <ryanbzhou@tencent.com>
2025-03-09 16:39:32 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  // 项目配置
-  output: 'server',
-  adapter: cloudflare()
 }); 
